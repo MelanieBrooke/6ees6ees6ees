ecec74211c964479bf0bb3a72cb6ddb16c5eb0c4
Javier De La Fuente <javierdelafuentejr@gmail.com>
2020-12-02 20:28:42 -0600

    Complete Bare Minimum Requirements

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,10 +1,10 @@
 class ForagerBee extends Bee {
   constructor() {
-    super();
-    this.age: 10;
-    this.job: 'make pollen';
-    this.canFly: true;
-    this.treasureChest: [];
+    super()
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
   }
 
   forage(treasure) {
