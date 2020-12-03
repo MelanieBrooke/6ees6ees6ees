b37896059188cd9dbb373ad256bf5ca56e2c9986
Javier De La Fuente <javierdelafuentejr@gmail.com>
2020-12-02 20:10:12 -0600

    Complete Bee

--- src/Grub.js
+++ src/Grub.js
@@ -1,8 +1,8 @@
 class Grub {
-  constructor(age, color, food) {
-    this.age = 0;
-    this.color = 'pink';
-    this.food = 'jelly';
+  constructor() {
+    this.age = 0,
+    this.color = 'pink',
+    this.food = 'jelly'
   }
 
   eat() {
