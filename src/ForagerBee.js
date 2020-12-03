b6fc5861e6fbb56857f7b52ae69616a14b5be3f2
Javier De La Fuente <javierdelafuentejr@gmail.com>
2020-12-02 20:21:54 -0600

    Complete HoneyMakerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age: 10;
+    this.job: 'make pollen';
+    this.canFly: true;
+    this.treasureChest: [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
